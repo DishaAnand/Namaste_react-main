df5975fe70901c4cd14a76d7756c5c3fbb2abe39
Disha Anand <akanksha010605@gmail.com>
2023-08-01 11:25:20 +0530

    tailwind-css

--- src/components/Body/RestroCard.js
+++ src/components/Body/RestroCard.js
@@ -1,13 +1,13 @@
 
-import {IMG_CDN_URL} from '../../shared/config';
+import { IMG_CDN_URL } from '../../shared/config';
 
-const RestroCard =({name,cuisines,lastMileTravelString,cloudinaryImageId}) =>{
+const RestroCard = ({ name, cuisines, lastMileTravelString, cloudinaryImageId }) => {
 
   return (
-    <div className = "card">
-      <img src ={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
-    +cloudinaryImageId} />
-       <h2>{name}</h2>
+    <div className="w-[200px] p-2 m-2 shadow-lg bg-pink-50">
+      <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+        + cloudinaryImageId} />
+      <h2 className='font-bold text-xl'>{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
       <h4>{lastMileTravelString} minutes</h4>
     </div>
